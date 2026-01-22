059544ffec2843c750e123546fcb37e60f758ab1
AADARSH028 <adar843462@gmail.com>
2026-01-22 20:23:50 +0530

    Initial ML Sales Prediction App

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request
+import joblib
+import numpy as np
+
+app = Flask(__name__)
+
+model = joblib.load("Admodel.pkl")
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    try:
+        features = [float(x) for x in request.form.values()]
+        final_features = np.array(features).reshape(1, -1)
+        prediction = model.predict(final_features)
+
+        return render_template(
+            "index.html",
+            prediction_text=f"Predicted Value: {prediction[0]}"
+        )
+    except Exception as e:
+        return render_template(
+            "index.html",
+            prediction_text="Invalid input"
+        )
+
+if __name__ == "__main__":
+    app.run(debug=True)
